f4e3656a1b93a82b20c50cea1dae1a05688a47ce
John Parks <johnwparks03@gmail.com>
2026-09-11 14:48:44 -0500

    Added findByEmail and existsByEmail to the UserRepository

--- src/main/java/assembly/general/api/repository/UserRepository.java
+++ src/main/java/assembly/general/api/repository/UserRepository.java
@@ -8,4 +8,7 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
