98e07a54f7d03c04d9b68eaccf7078f903c0b6e7
John Parks <johnwparks03@gmail.com>
2026-09-13 12:36:24 -0500

    Add functions to query user active reservations and borrowing history for profile

--- src/main/java/assembly/general/api/repository/ReservationRepository.java
+++ src/main/java/assembly/general/api/repository/ReservationRepository.java
@@ -2,10 +2,27 @@ package assembly.general.api.repository;
 
 import assembly.general.api.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    @Query("""
+    SELECT COUNT(r)
+    FROM Reservation r
+    WHERE r.user.id = :userId
+      AND r.status IN ('RESERVED', 'CHECKED_OUT')
+    """)
+    Long getUserActiveReservations(@Param("userId") UUID userId);
+
+    @Query("""
+    SELECT COUNT(r)
+    FROM Reservation r
+    WHERE r.user.id = :userId
+      AND r.status IN ('RETURNED')
+    """)
+    Long getUserBorrowingHistory(@Param("userId") UUID userId);
 }
